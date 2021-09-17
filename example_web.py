a531c4cb1474b75436da3b2a934a0f0acdb09051
veikai <weikai.zhu@njtf.cn>
2021-09-17 10:02:28 +0800

    init

--- example_web.py
+++ example_web.py
@@ -3,24 +3,36 @@ from flask.json import jsonify, request
 from time import sleep
 from flask_cors import CORS
 import random
+from queue import Queue
+from threading import Thread
 
 
 app = SpyService(__name__, key="ba31e59e9574332cdc7ee6198a725c70")
 CORS(app, supports_credentials=True)  # 允许跨域
+send_queue = Queue()
 
 
-@app.route("/send_text")
+def send():
+    while True:
+        wxid, msg = send_queue.get()
+        app.spy.send_text(wxid, msg)
+        sleep(random.randint(3, 5))
+
+
+t = Thread(target=send)
+t.daemon = True
+t.start()
+
+
+@app.route("/send_text", methods=["GET", "POST"])
 def send_text():
     nicknames = request.json["nicknames"]
     msg = request.json["msg"]
-    # nicknames = ["晨风", "720", "大管家群发测试"]
-    # msg = "test"
     for nickname in nicknames:
         wxid = app.nickname2wxid.get(nickname)
-        app.spy.send_text(wxid, msg)
-        sleep(random.randint(3, 5))
+        send_queue.put((wxid, msg))
     return jsonify({"code": 1})
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0")
